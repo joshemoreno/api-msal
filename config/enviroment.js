38b01ec5debd0c68a49542e594104671a7fb5a21
joshemoreno <60795066+joshemoreno@users.noreply.github.com>
2021-10-09 14:54:30 -0500

    change credentials

--- config/enviroment.js
+++ config/enviroment.js
@@ -1,24 +1,15 @@
 module.exports = {
     // endPoint's
-    entryPoint: 'https://login.microsoftonline.com/16e7cf3f-6af4-4e76-941e-aecafb9704e9/saml2',
-    entityURI: 'https://sts.windows.net/16e7cf3f-6af4-4e76-941e-aecafb9704e9',
+    entryPoint: 'YOUR URL MICROSOFT',
+    entityURI: 'ENTRY POINT URI MICROSOFT',
     
     // About Host
     host: 'http://localhost',
     port: 3000,
     
     // Other's
-    idAppAzure: '9f05411d-952b-457f-abda-f9c8b6af1251',
-    authority: 'https://login.microsoftonline.com/16e7cf3f-6af4-4e76-941e-aecafb9704e9',
-    secret :'QVmsxAcs-i5k4G7-X.d46gCIV-8x7J_0NL',
-    
-    idAppAzure2: 'a0512cd9-c401-472c-9257-9f3487308473',
-    authority2: 'https://login.microsoftonline.com/common',
-    secret2: '~DbpHaUnr6p2pieB88Xpm~_E4Lk_91hEvP',
-
-    idAppAzure3: '6ce3f510-883b-43ac-89be-72eaeb506469',
-    authority3: 'https://login.microsoftonline.com/common',
-    secret3: '~Z-P.u2YaMT~SS2G3rbO4P42u1j6n_47gw',
-    
-    cert: 'MIIDBTCCAe2gAwIBAgIQN33ROaIJ6bJBWDCxtmJEbjANBgkqhkiG9w0BAQsFADAtMSswKQYDVQQDEyJhY2NvdW50cy5hY2Nlc3Njb250cm9sLndpbmRvd3MubmV0MB4XDTIwMTIyMTIwNTAxN1oXDTI1MTIyMDIwNTAxN1owLTErMCkGA1UEAxMiYWNjb3VudHMuYWNjZXNzY29udHJvbC53aW5kb3dzLm5ldDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKGiy0/YZHEo9rRn2bI27u189Sq7NKhInFz5hLCSjgUB2rmf5ETNR3RJIDiW1M51LKROsTrjkl45cxK6gcVwLuEgr3L1TgmBtr/Rt/riKyxeXbLQ9LGBwaNVaJrSscxfdFbJa5J+qzUIFBiFoL7kE8ZtbkZJWBTxHEyEcNC52JJ8ydOhgvZYykete8AAVa2TZAbg4ECo9+6nMsaGsSBncRHJlRWVycq8Q4HV4faMEZmZ+iyCZRo2fZufXpn7sJwZ7CEBuw4qycHvUl6y153sUUFqsswnZGGjqpKSq7I7sVI9vjB199RarHaSSbDgL2FxjmASiUY4RqxnTjVa2XVHUwUCAwEAAaMhMB8wHQYDVR0OBBYEFI5mN5ftHloEDVNoIa8sQs7kJAeTMA0GCSqGSIb3DQEBCwUAA4IBAQBnaGnojxNgnV4+TCPZ9br4ox1nRn9tzY8b5pwKTW2McJTe0yEvrHyaItK8KbmeKJOBvASf+QwHkp+F2BAXzRiTl4Z+gNFQULPzsQWpmKlz6fIWhc7ksgpTkMK6AaTbwWYTfmpKnQw/KJm/6rboLDWYyKFpQcStu67RZ+aRvQz68Ev2ga5JsXlcOJ3gP/lE5WC1S0rjfabzdMOGP8qZQhXk4wBOgtFBaisDnbjV5pcIrjRPlhoCxvKgC/290nZ9/DLBH3TbHk8xwHXeBAnAjyAqOZij92uksAv7ZLq4MODcnQshVINXwsYshG1pQqOLwMertNaY5WtrubMRku44Dw7R',
+    idAppAzure: 'ID APP AZURE AD',
+    authority: 'URL AUTHORITY',
+    secret :'SECRET CLIEND APP AZURE AD',
+  
   }
